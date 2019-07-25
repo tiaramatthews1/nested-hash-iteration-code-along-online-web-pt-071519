16a9c856a6aa33ba83fcc2e25e19c2c4688c3452
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-25 13:35:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,4 +5,10 @@ contacts.each do |person, contact_details_hash|
   binding.pry
 end
 
-
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
+  end
+end
