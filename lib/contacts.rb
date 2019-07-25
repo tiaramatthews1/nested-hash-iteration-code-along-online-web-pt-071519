1d8855cae6bd24ac7954a39993fe362a93ca51d4
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-25 13:39:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,15 +1,3 @@
   require 'pry'
 
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  binding.pry
-end
-
-def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      binding.pry
-    end
-  end
-end
